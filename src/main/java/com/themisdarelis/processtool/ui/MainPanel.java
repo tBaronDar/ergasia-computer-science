1c2113d4355ba0ca5c1a554c8d1c8a5991db8ce4
tBaronDar <thedarelis@gmail.com>
2025-11-15 12:24:17 +0200

    changed layout, introduced side panel for text and links

--- src/main/java/com/themisdarelis/processtool/ui/MainPanel.java
+++ src/main/java/com/themisdarelis/processtool/ui/MainPanel.java
@@ -8,27 +8,92 @@ import com.themisdarelis.processtool.service.ProcessManager;
 public class MainPanel extends JPanel {
     public MainPanel() {
         setBorder(new EmptyBorder(24, 32, 24, 32));
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        add(new JLabel("Welcome to Process Tool"));
+        setLayout(new BorderLayout(12, 12));
 
-        var startButton=new JButton("Start Process");
-        startButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+        // Welcome label
+        JLabel welcomeLabel = new JLabel("Welcome to Process Tool");
+        welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        welcomeLabel.setFont(welcomeLabel.getFont().deriveFont(Font.BOLD, 32f));
+        add(welcomeLabel, BorderLayout.NORTH);
 
+        // Keimena
+        JPanel descriptionPanel = new JPanel();
+        descriptionPanel.setLayout(new BoxLayout(descriptionPanel, BoxLayout.Y_AXIS));
+        descriptionPanel.setBorder(new EmptyBorder(8, 8, 8, 8));
+        JLabel descLine1 = new JLabel("Περιγραφή: Αυτό το πρόγραμμα διαχειρίζεται GUI διεργασίες. Γραψτε το όνομα μιας GUI διεργασίας στο πεδίο κειμένου παρακάτω και πατήστε 'Start Process' για να την ξεκινήσετε. Επιλέξτε μια διεργασία από τον πίνακα και πατήστε 'Kill Process' για να την τερματίσετε.");
+        descLine1.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JLabel descLine2 = new JLabel("Author: Themis Darelis");
+        descLine2.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JLabel githubLabel = new JLabel("<html>GitHub: <a href='https://github.com/tBaronDar/ergsia-computer-science'>https://github.com/user/repo</a></html>");
+        githubLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        descriptionPanel.add(descLine1);
+        descriptionPanel.add(Box.createRigidArea(new Dimension(0,6)));
+        descriptionPanel.add(descLine2);
+        descriptionPanel.add(Box.createRigidArea(new Dimension(0,6)));
+        descriptionPanel.add(githubLabel);
+
+        // pinaka kai modelo
         var processList = ProcessManager.getCurrentProcesses();
-        // δημιουργία μοντέλου πίνακα και πίνακα
         ProcessInfoTableModel tableModel = new ProcessInfoTableModel(processList);
         JTable processTable = new JTable(tableModel);
         processTable.setRowHeight(32);
         processTable.setRowSelectionAllowed(true);
-        JScrollPane scrollPane = new JScrollPane(processTable);
-        // me to pou valeis to scrollpane kane scroll down
-        //den douleuei auto....
-        SwingUtilities.invokeLater(() -> scrollToBottom(scrollPane));
-        add(scrollPane);
+        processTable.setPreferredScrollableViewportSize(new Dimension(500, 500));
+        processTable.setMinimumSize(new Dimension(500, 500));
 
-        var textInput= new JTextField();
+        JScrollPane scrollPane = new JScrollPane(processTable);
+        scrollPane.setMinimumSize(new Dimension(500, 500));
+
+        // split panel aristera ta keimena deksia o pinakas
+        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, descriptionPanel, scrollPane);
+        split.setResizeWeight(0.25);
+        split.setOneTouchExpandable(true);
+        add(split, BorderLayout.CENTER);
+
+        // kato panel
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.Y_AXIS));
+        bottomPanel.setBorder(new EmptyBorder(8, 0, 0, 0));
+
+        // input
+        var textInput = new JTextField();
         textInput.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));
-        add(textInput);
+        textInput.setAlignmentX(Component.LEFT_ALIGNMENT);
+        bottomPanel.add(textInput);
+
+        bottomPanel.add(Box.createRigidArea(new Dimension(0,8)));
+
+        // 2 rows gia koubia
+        JPanel buttonRows = new JPanel();
+        buttonRows.setLayout(new GridLayout(2, 1, 6, 6));
+
+        JPanel row1 = new JPanel(new FlowLayout(FlowLayout.LEFT, 8, 0));
+        var startButton = new JButton("Start Process");
+        var killButton = new JButton("Kill Process");
+        row1.add(startButton);
+        row1.add(killButton);
+
+        JPanel row2 = new JPanel(new FlowLayout(FlowLayout.LEFT, 8, 0));
+        var refreshPlaceholder = new JButton("Refresh");
+        refreshPlaceholder.setEnabled(false);
+        var exitPlaceholder = new JButton("Exit");
+        exitPlaceholder.setEnabled(false);
+        row2.add(refreshPlaceholder);
+        row2.add(exitPlaceholder);
+
+        buttonRows.add(row1);
+        buttonRows.add(row2);
+        buttonRows.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        bottomPanel.add(buttonRows);
+
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        // Scroll to bottom, distihos auto den douleuei on start up
+        //den xero giati...
+        SwingUtilities.invokeLater(() -> scrollToBottom(scrollPane));
+
+        // listeners
         startButton.addActionListener(e -> {
             String input = textInput.getText().trim();
             if (!input.isEmpty()) {
@@ -47,10 +112,6 @@ public class MainPanel extends JPanel {
                 JOptionPane.showMessageDialog(this, "Please the name of a gui program.", "Warning", JOptionPane.WARNING_MESSAGE);
             }
         });
-        add(startButton);
-
-        var killButton=new JButton("Kill Process");
-        killButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
 
         killButton.addActionListener(e -> {
             int selectedRow = processTable.getSelectedRow();
@@ -70,7 +131,6 @@ public class MainPanel extends JPanel {
                 JOptionPane.showMessageDialog(this, "Please select a program from the list.", "Warning", JOptionPane.WARNING_MESSAGE);
             }
         });
-        add(killButton);
 
     }
 
